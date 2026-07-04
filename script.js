d22bc047ae4c495d20257b06ea5f1944a0cc587b
Research Portal Developer <dev@research.local>
2026-07-04 15:03:16 +0530

    Restore homepage files

---
+++ script.js
@@ -0,0 +1,15 @@
+
+ function toggleMent() {
+    let menu = document.getElementById("menu-list");
+
+    if (menu.style.display === "flex"
+        
+    ) {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+   
+let ic = document.querySelector(".menu-icon");
+}
+toggleMenu();
